72fb20db43c09d8ca6f63cbbade493b6635f5a0b
DaniM0613 <wwh.rj.92@gmail.com>
2024-02-11 15:48:14 -0600

    confirmar cuenta

--- frontend/src/paginas/ConfirmarCuenta.jsx
+++ frontend/src/paginas/ConfirmarCuenta.jsx
@@ -1,6 +1,14 @@
+import { useEffect, useState} from 'react'
+import { useParams, Link } from 'react-router-dom'
+import axios from 'axios'
+import Alerta from '../components/Alerta'
 
 
 const ConfirmarCuenta = () => {
+
+  const params = useParams();
+
+  console.log(params)
   return (
     <>
        <h1 className='text-purple-900 text-5xl capitalize font-black'>Confirma tu cuenta y 
