afbc6d595381d2d568ff6791aacf358ada0dc897
DaniM0613 <wwh.rj.92@gmail.com>
2024-02-29 20:51:36 -0600

    upload

--- backend/routes/proyectoRoutes.js
+++ backend/routes/proyectoRoutes.js
@@ -7,8 +7,9 @@ import {
     editarProyecto,
     elimiarProyecto,
     agregarColaborador,
+    buscarColaborador,
     eliminarColaborador,
-    obtenerTareas,
+
 
 } from '../controllers/proyectoController.js'
 import checkAuth from '../middleware/checkAuth.js'
@@ -26,8 +27,8 @@ router
  .put(checkAuth, editarProyecto)
  .delete(checkAuth, elimiarProyecto);
 
-router.get('/tareas/:id', checkAuth, obtenerTareas);
-router.post('/agregar-colaborador/:id', checkAuth, agregarColaborador)
-router.post('/eliminar-colaborador/:id', checkAuth, eliminarColaborador)
+router.post('/colaboradores', checkAuth, buscarColaborador)
+router.post('/colaboradores/:id', checkAuth, agregarColaborador)
+router.delete('/colaboradores/:id', checkAuth, eliminarColaborador)
 
 export default router;
